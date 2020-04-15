0ebeec8939d8471858bde14f92d4ad785fe7ff19
Gitmaster99 <wvanbalen@gmail.com>
2020-04-15 21:49:47 +0200

    changed the whole home-page

--- Javascript Webpage OP3/Pen-tests/app.js
+++ Javascript Webpage OP3/Pen-tests/app.js
@@ -1,8 +1,40 @@
 
 
-function classToggle() {
-  var el = document.querySelector('.icon-cards__content');
-  el.classList.toggle('step-animation');
+// function classToggle() {
+//   var el = document.querySelector('.icon-cards__content');
+//   el.classList.toggle('step-animation');
+// }
+
+// document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+
+
+
+
+
+function increment(){
+  $('.box').hide();
+  setTimeout(function(){
+    $('.box').show();
+  },100);
+
+  $('.number').each(function(){
+    var $this = $(this),
+        value = $this.text();
+    $({someValue: 0}).animate({someValue: value}, {
+      duration: 1200,
+      easing:'swing', 
+      step: function() {      
+        $this.text(Math.round(this.someValue));
+      },
+      complete: function() {
+        $this.text(value);
+      }
+    });
+  });  
 }
 
-document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+increment(); 
+
+setInterval(function(){
+  increment();
+},2000);
