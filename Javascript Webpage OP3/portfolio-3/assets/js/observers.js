6126abb7b73a214b033748ce757588ea85d2863a
Gitmaster99 <wvanbalen@gmail.com>
2020-04-21 09:06:20 +0200

    changes made

---
+++ Javascript Webpage OP3/portfolio-3/assets/js/observers.js
@@ -0,0 +1,105 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+  rootMargin: "-1200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+
+document.getElementById("toggle").addEventListener("click", changeColor);
+
+isClicked = true;
+
+function changeColor() {
+  if(isClicked){
+  document.getElementById("boo").classList.remove("hide");
+     document.getElementById("boo").classList.add("show");
+    isClicked=false;
+  } else{
+  document.getElementById("boo").classList.remove("show");
+     document.getElementById("boo").classList.add("hide");
+    isClicked=true;
+  }
+}
+
+
+
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
+
+
+
+
+
+
+
